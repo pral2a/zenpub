8aa904c72e1c9afcff0c3fbda909e98ad5a773f5
James Laver <james.laver@gmail.com>
2019-06-26 14:47:14 +0200

    Comment out unused reference to unknown function in not yet used activity pub view to avoid an error

--- lib/activity_pub_web/views/activity_pub_view.ex
+++ lib/activity_pub_web/views/activity_pub_view.ex
@@ -68,7 +68,7 @@ defmodule ActivityPubWeb.ActivityPubView do
     entity
     |> Entity.extension_fields()
     |> Enum.filter(&filter_by_value/1)
-    |> Enum.map(&map_value/1)
+    # |> Enum.map(&map_value/1)
   end
 
   # FIXME this can be calculated in compilation time :)
