c7deebeae973aca53628a68d665384ba1c4b7af2
Karen Konou <konoukaren@gmail.com>
2019-08-28 15:16:45 +0200

    Add documentation

--- lib/activity_pub/webfinger.ex
+++ lib/activity_pub/webfinger.ex
@@ -4,12 +4,19 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule ActivityPub.WebFinger do
+  @moduledoc """
+  Serves and fetches data (mainly actor URI) necessary for federation when only the username and host is known.
+  """
+
   alias ActivityPub.Actor
   alias ActivityPub.HTTP
   alias ActivityPubWeb.Federator.Publisher
 
   require Logger
 
+  @doc """
+  Serves a webfinger response for the requested username.
+  """
   def webfinger(resource) do
     host = System.get_env("HOSTNAME") || MoodleNetWeb.Endpoint.host()
     regex = ~r/(acct:)?(?<username>[a-z0-9A-Z_\.-]+)@#{host}/
@@ -20,6 +27,7 @@ defmodule ActivityPub.WebFinger do
     else
       _e ->
         actor = ActivityPub.get_by_id(resource, aspect: :actor)
+
         if actor do
           {:ok, represent_user(actor)}
         else
@@ -38,6 +46,9 @@ defmodule ActivityPub.WebFinger do
     ] ++ Publisher.gather_webfinger_links(actor)
   end
 
+  @doc """
+  Formats gathered data into a JRD format.
+  """
   def represent_user(actor) do
     {:ok, actor} = ActivityPub.Utils.ensure_keys_present(actor)
 
@@ -80,6 +91,9 @@ defmodule ActivityPub.WebFinger do
     {:ok, data}
   end
 
+  @doc """
+  Fetches webfinger data for an account given in "@username@domain.tld" format.
+  """
   def finger(account) do
     account = String.trim_leading(account, "@")
 
