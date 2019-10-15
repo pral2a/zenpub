00e0db8b8db172490f671ee0a84c127176670bad
James Laver <james.laver@gmail.com>
2019-10-15 13:29:33 +0200

    bump versions of this app, erlang and elixir

--- mix.exs
+++ mix.exs
@@ -5,8 +5,8 @@ defmodule MoodleNet.Mixfile do
   def project do
     [
       app: :moodle_net,
-      version: "0.9.5-dev",
-      elixir: "~> 1.9.0",
+      version: "1.0.0-dev",
+      elixir: "~> 1.9.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
